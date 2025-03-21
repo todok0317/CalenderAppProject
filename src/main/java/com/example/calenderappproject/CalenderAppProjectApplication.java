9107b687dedb406275c7ac5e58fc3099afbcfa30
todok0317 <todok0317@naver.com>
2025-03-21 09:32:40 +0900

    Initial commit

---
+++ src/main/java/com/example/calenderappproject/CalenderAppProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.calenderappproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalenderAppProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalenderAppProjectApplication.class, args);
+    }
+
+}
